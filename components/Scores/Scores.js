d4cb330c4b99e83de0cc0d39177d646029ca1b90
Sherwin Zhang <sherwinzhang100@gmail.com>
2021-11-09 17:31:11 -0800

    component fetch success

--- components/Scores/Scores.js
+++ components/Scores/Scores.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
 import fetch from 'isomorphic-unfetch';
-import MusicScore from '../Music'
+import {MusicScore} from '../Music'
 
 // function Scores(props) {
 //     return <div> reached </div>
@@ -9,9 +9,13 @@ import MusicScore from '../Music'
 
 
 
-const Scores = ({ scores }) => {
-    console.log({scores})
-    console.log("reached")
+// const Scores = ({ scores }) => {
+//     console.log({scores})
+function Scores(props) {
+    let scores = props.scorekey
+    console.log("Scores.js component")
+    console.log(scores)
+    //return <div> this is my component </div>
     return (
         <div className="notes-container">
             <h1>Scores</h1>
@@ -20,11 +24,12 @@ const Scores = ({ scores }) => {
                     return (
                         <div key={note._id}>
                             {note._id}
+                            {' \n '}
                             {note.description}
                             <MusicScore
                                 title={note.title}
                                 abc={note.music}
-                            />
+                                />
                         </div>
                     )
                 })}
@@ -33,13 +38,13 @@ const Scores = ({ scores }) => {
     )
 }
 
-Scores.getInitialProps = async () => {
-    const res = await fetch('http://localhost:3000/api/scores');
-    const { data } = await res.json();
-
-    console.log("fetch this")
-    console.log(data)
-
-    return { scores: data }
-}
+// Scores.getInitialProps = async () => {
+//     const res = await fetch('http://localhost:3000/api/scores');
+//     const { data } = await res.json();
+//
+//     console.log("fetch this")
+//     console.log(data)
+//
+//     return { scores: data }
+// }
 export default Scores
